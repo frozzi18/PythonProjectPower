450d7bed15b6eb74a84953126a7d735846c55f66
frozzi18 <42233054+frozzi18@users.noreply.github.com>
2019-04-26 17:05:43 +0200

    Adding Exercise 2

--- foo_corporation_exercise_1.py
+++ foo_corporation_exercise_1.py
@@ -15,10 +15,19 @@ class Manager(Employee):
         self.officeNumber = office_number
 
     def office_number(self):
-        print(self.name, self.officeNumber)
+        print("The office number for ", self.name, " is ", self.officeNumber)
 
 
+John = Employee("John", 7.5, 38)
+Annabel = Employee("Annabel", 8.2, 42)
+Graham = Employee("Graham", 10.50, 41)
+Bill = Manager("Bill", 15.50, 39, "A332")
+Gregory = Manager("Gregory", 18.20, 40, "A415")
 
-John = Manager("John", 7.5, 38, "rozzi")
 John.salary()
-John.office_number()
+Annabel.salary()
+Graham.salary()
+Bill.salary()
+Bill.office_number()
+Gregory.salary()
+Gregory.office_number()
